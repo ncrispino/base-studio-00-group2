80397f7f6b6d62875287da3246e8a334ec1f9be3
c.zhang <c.zhang@localhost>
2022-01-20 12:33:52 -0600

    Read the message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,6 +4,7 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
-		System.out.print("this is a test of the intercom system");
+		System.out.println("this is a test of the intercom system");
+		System.out.println("Hello from the other side!!");
 	}
 }
